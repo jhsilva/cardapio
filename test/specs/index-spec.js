22fc8fb2d2e7ba1001a9c124f3d470037045f038
Jhonatan da Silva Batista <admin@jhonatansilva.com>
2016-09-07 00:49:47 -0300

    feat: tests

---
+++ test/specs/index-spec.js
@@ -0,0 +1,12 @@
+var mocha = require('mocha');
+var chai = require('chai');
+var expect = chai.expect;
+
+describe('Test', function() {
+  describe('Zebeleo', function() {
+    it('open', function() {
+      var open = 'never';
+      expect(open).to.be.equal('never');
+    })
+  })
+});
